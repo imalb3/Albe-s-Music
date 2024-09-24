ee7e298e1b951da135e191a9ef60c7aa5a5a4e14
imalb3 <163497756+imalb3@users.noreply.github.com>
2024-09-24 13:25:32 +0200

    Update support.js

--- commands/support.js
+++ commands/support.js
@@ -36,11 +36,11 @@ module.exports = {
         const embed = new EmbedBuilder()
             .setColor('#ff4e4e')
             .setAuthor({
-              name: 'Support Server',
-              iconURL: 'https://cdn.discordapp.com/attachments/1230824451990622299/1230824519220985896/6280-2.gif?ex=6638ae28&is=66375ca8&hm=13e4a1b91a95b2934a39de1876e66c11711c7b30ac1a91c2a158f2f2ed1c2fc6&', 
+              name: 'Supporto Server',
+              iconURL: 'https://imgur.com/sCiXLgs.png', 
               url: 'https://discord.gg/xQF9f9yUEM'
           })
-            .setDescription(`➡️ **Join our Discord server for support and updates:**\n- Discord - ${supportServerLink}\n\n➡️ **Follow us on:**\n- GitHub - ${githubLink}\n- Replit - ${replitLink}\n- YouTube - ${youtubeLink}`)
+            .setDescription(`➡️ **Unisciti al nostro server Discord per supporto e aggiornamenti:**\n- Discord - ${supportServerLink}`)
             .setThumbnail(client.user.displayAvatarURL())
             .setTimestamp();
 
