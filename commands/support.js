f9748314e86488dcbc7172c1e3e4b6d0a179c2a9
imalb3 <163497756+imalb3@users.noreply.github.com>
2024-09-24 13:21:48 +0200

    Update support.js

--- commands/support.js
+++ commands/support.js
@@ -23,18 +23,18 @@ const { EmbedBuilder } = require('discord.js');
 
 module.exports = {
   name: "support",
-  description: "Get support server link",
+  description: "Ottieni il collegamento al server di supporto",
   permissions: "0x0000000000000800",
   options: [],
   run: async (client, interaction) => {
     try {
 
-      const supportServerLink = "https://discord.gg/xQF9f9yUEM";
+      const supportServerLink = "https://discord.gg/Un3FzMYvKc";
       const githubLink = "https://github.com/GlaceYT";
       const replitLink = "https://replit.com/@GlaceYT";
       const youtubeLink = "https://www.youtube.com/@GlaceYT";
         const embed = new EmbedBuilder()
-            .setColor('#b300ff')
+            .setColor('#ff4e4e')
             .setAuthor({
               name: 'Support Server',
               iconURL: 'https://cdn.discordapp.com/attachments/1230824451990622299/1230824519220985896/6280-2.gif?ex=6638ae28&is=66375ca8&hm=13e4a1b91a95b2934a39de1876e66c11711c7b30ac1a91c2a158f2f2ed1c2fc6&', 
