fcd21f39ba8e24496c5a055203f5ddc2352f0d54
imalb3 <163497756+imalb3@users.noreply.github.com>
2024-09-24 13:23:17 +0200

    Update support.js

--- commands/support.js
+++ commands/support.js
@@ -41,7 +41,7 @@ module.exports = {
               url: 'https://discord.gg/xQF9f9yUEM'
           })
             .setDescription(`➡️ **Join our Discord server for support and updates:**\n- Discord - ${supportServerLink}\n\n➡️ **Follow us on:**\n- GitHub - ${githubLink}\n- Replit - ${replitLink}\n- YouTube - ${youtubeLink}`)
-            .setImage('https://cdn.discordapp.com/attachments/1113800537402527903/1236803979996958740/11.png?ex=663956f7&is=66380577&hm=3b3c19a11adcb979517a133f2907f671305d23f1f5092cf7df043e6d5cab07bc&')
+            .setThumbnail(client.user.displayAvatarURL())
             .setTimestamp();
 
       return interaction.reply({ embeds: [embed] });
