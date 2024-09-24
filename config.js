5e95a9e02a695028577095ee39e116a3379d95c1
imalb3 <163497756+imalb3@users.noreply.github.com>
2024-09-24 12:48:37 +0200

    Update config.js

--- config.js
+++ config.js
@@ -23,14 +23,14 @@
 
 module.exports = {
   TOKEN: "",
-  ownerID: ["1004206704994566164", ""], 
+  ownerID: ["420873238378184704", ""], 
   setupFilePath: './commands/setup.json',
   commandsDir: './commands',  
   embedColor: "#ff4e4e",
   musicardTheme:"themes16", //Goes from themes1 to themes19
   activityName: "Albe's Music", // This is bot status Write Anything here 
   activityType: "WATCHING",  // Available activity types : LISTENING , PLAYING
-  SupportServer: "https://discord.gg/xQF9f9yUEM",
+  SupportServer: "https://discord.gg/Un3FzMYvKc",
   CheckmarkIcon: "https://cdn.discordapp.com/attachments/1230824451990622299/1230836684774576168/7762-verified-blue.gif",
   MusicIcon:"https://media.discordapp.net/attachments/1230824451990622299/1236664581364125787/music-play.gif",
   embedTimeout: 5,  // Timeout before the button interaction embeds are deleted ( Default - 5 seconds)
