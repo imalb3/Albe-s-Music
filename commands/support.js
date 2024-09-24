4f73f7eae01efd5f4cd88d1ab95bf4e3b18c30e3
imalb3 <163497756+imalb3@users.noreply.github.com>
2024-09-24 13:26:12 +0200

    Update support.js

--- commands/support.js
+++ commands/support.js
@@ -30,9 +30,6 @@ module.exports = {
     try {
 
       const supportServerLink = "https://discord.gg/Un3FzMYvKc";
-      const githubLink = "https://github.com/GlaceYT";
-      const replitLink = "https://replit.com/@GlaceYT";
-      const youtubeLink = "https://www.youtube.com/@GlaceYT";
         const embed = new EmbedBuilder()
             .setColor('#ff4e4e')
             .setAuthor({
